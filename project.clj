d18416aab7b72c075103ec5b8366ab711dd000ed
ilevd <ilevd@mail.ru>
2022-05-13 13:04:58 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compile-time "1.0.0"
+(defproject ilevd/compile-time "1.0.0"
   :description "Run Clojure code in compile time"
   :url "https://github.com/ilevd/compile-time"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
