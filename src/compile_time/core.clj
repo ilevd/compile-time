b63f30ab00a667fd4aee21af49335f0b12e89a85
ilevd <ilevd@mail.ru>
2022-05-14 08:44:21 +0300

    Update doc strings

--- src/compile_time/core.clj
+++ src/compile_time/core.clj
@@ -2,14 +2,15 @@
 
 
 (defmacro run
-  "Run forms in compile time, return result of last forms"
+  "Pass body to evaluate it at compile time.
+  All forms will be evaluated and the result of evaluating the last form will be returned."
   [& forms]
   (run! eval (butlast forms))
   (eval (last forms)))
 
 
 (defmacro run-fn
-  "Run function with args in compile time"
+  "Pass function and args to evaluate it at compile time."
   [f & args]
   (if (symbol? f)
     (apply (resolve f) args)
